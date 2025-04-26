96b75beea126c32226d82e840b8703b5361f194f
nhat thang <nhtthng2812@gmail.com>
2025-04-26 18:32:53 +0700

    Add CMake configuration and build files for x86_64 and arm64-v8a architectures
    - Created build_file_index.txt to track build files for x86_64.
    - Added cmake_install.cmake for installation configuration.
    - Generated configure_fingerprint.bin for structured logging.
    - Created metadata_generation_command.txt with build parameters.
    - Added prefab_config.json to manage prefab settings.
    - Updated symbol_folder_index.txt with symbol folder paths.
    - Initialized cache files for arm64-v8a architecture.
    - Generated hash_key.txt for hash calculation in build folder.

--- lib/Screen/UpdateInformation.dart
+++ lib/Screen/UpdateInformation.dart
@@ -12,6 +12,7 @@ class _UpdateInformationState extends State<UpdateInformation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       appBar: AppBar(
         title: Text(
           'Cập nhật hồ sơ',
@@ -77,10 +78,24 @@ class _UpdateInformationState extends State<UpdateInformation> {
             ),
             const SizedBox(height: 20),
             Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20),
-              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 10.0,
+                    spreadRadius: 5.0,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  
                   Text('Thông tin cá nhân',
                       style: TextstyleHelper.nunito(
                           fontSize: 20,
@@ -89,6 +104,7 @@ class _UpdateInformationState extends State<UpdateInformation> {
                   const SizedBox(height: 20),
                   Form(
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
                           'Tên hiển thị',
@@ -164,12 +180,100 @@ class _UpdateInformationState extends State<UpdateInformation> {
                             ),
                           ),
                         ),
+                        const SizedBox(height: 30),
+                        Text('Địa chỉ',
+                            style: TextstyleHelper.nunito(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black)),
+                                const SizedBox(height: 20),
+
+                        Text('Tỉnh/Thành phố',
+                            style: TextstyleHelper.nunito(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black.withOpacity(0.5))),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.location_on),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(
+                                  10.0), // Thay đổi giá trị này để điều chỉnh độ cong
+                              borderSide: const BorderSide(
+                                  color: Colors.grey), // Màu viền
+                            ),
+                            hintText: 'Nhập tỉnh/thành phố',
+                            hintStyle: TextstyleHelper.nunito(
+                              fontSize: 11,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text('Quận/Huyện',
+                            style: TextstyleHelper.nunito(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black.withOpacity(0.5))),
+                        const SizedBox(height: 10),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: const Icon(Icons.location_city),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(
+                                  10.0), // Thay đổi giá trị này để điều chỉnh độ cong
+                              borderSide: const BorderSide(
+                                  color: Colors.grey), // Màu viền
+                            ),
+                            hintText: 'Nhập quận/huyện',
+                            hintStyle: TextstyleHelper.nunito(
+                              fontSize: 11,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
                 ],
               ),
-            )
+            ),
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Xử lý khi nhấn nút "Cập nhật"
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[900],
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 100, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.check_circle,
+                        color: Colors.white, size: 20),
+                    const SizedBox(width: 10),
+                    Text(
+                      'Cập nhật',
+                      style: TextstyleHelper.nunito(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+                
+              ),
+            ),
+            const SizedBox(height: 30),
           ],
         ),
       ),
